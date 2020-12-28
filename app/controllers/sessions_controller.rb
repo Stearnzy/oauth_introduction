347c9908c8273f68661d01ef8dcbb5462fd0afaf
Zach Stearns <zach.stearns92@gmail.com>
2020-12-28 13:10:34 -0700

    create data response for user information using secret, id and code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,27 @@
 class SessionsController < ApplicationController
   def create
-    
+    client_id = '7021d177a4d68f042962'
+    client_secret = 'd335ab566d666a3421aa440f5786ed6eafec890b'
+    code = params[:code]
+
+    conn = Faraday.new(url: 'https://github.com', headers: { 'Accept': 'application/json' })
+
+    response = conn.post('/login/oauth/access_token') do |request|
+      request.params['code'] = code
+      request.params['client_id'] = client_id
+      request.params['client_secret'] = client_secret
+    end
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    access_token = data[:access_token]
+
+    conn = Faraday.new(
+      url: 'https://api.github.com',
+      headers: {
+        'Authorization': "token #{access_token}"
+      }
+    )
+    response = conn.get('/user')
+    data = JSON.parse(response.body, symbolize_names: true)
   end
 end
